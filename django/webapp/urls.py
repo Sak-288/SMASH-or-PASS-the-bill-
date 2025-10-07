cc2e42604e5344b9663964f82e40c4f5be1c07ae
Amine <aminesakoute288@gmail.com>
2025-10-07 13:42:53 +0100

    created sub "webapp" folders

--- django/webapp/urls.py
+++ django/webapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.home, name='home'),        # for root URL (e.g. /)
+    path('home/', views.home, name='home'),   # for /home URL
 ]
