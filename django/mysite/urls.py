940dd6e7f92d064ff2f68b0e5087f7fc4d1af551
Amine <aminesakoute288@gmail.com>
2025-10-07 13:38:48 +0100

    setting up home page [test w/ only panot]

--- django/mysite/urls.py
+++ django/mysite/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("polls/", include("webapp.urls")),
-    path("webapp/", admin.site.urls),
+    path("webapp/", include("webapp.urls")),
+    path("admin/", admin.site.urls),
 ]
