a368ac9b7b4a01535cf51a8f451231427b3eac2a
Amine <aminesakoute288@gmail.com>
2025-10-07 14:26:38 +0100

    templates --> static --> webapp/views --> webapp/ urls --> settings/INSTALLED_APPS

--- django/mysite/settings.py
+++ django/mysite/settings.py
@@ -44,7 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webapp',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
