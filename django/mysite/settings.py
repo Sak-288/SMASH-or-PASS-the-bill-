8b828c57114fbe3d62583655a5935d4fb347ee20
Amine <aminesakoute288@gmail.com>
2025-10-07 14:00:50 +0100

    correcting a few django-related issues

--- django/mysite/settings.py
+++ django/mysite/settings.py
@@ -54,10 +54,11 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [],  # keep empty if using app templates like webapp/templates/webapp/
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
