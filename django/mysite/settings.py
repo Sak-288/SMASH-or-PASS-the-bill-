683ad00c2d5e05038d4d68bc05a8f99291ef9138
Amine <aminesakoute288@gmail.com>
2025-10-10 23:29:17 +0100

    contact form SUCCESSFUL

--- django/mysite/settings.py
+++ django/mysite/settings.py
@@ -34,6 +34,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'aminesakoute288@gmail.com'
+EMAIL_HOST_PASSWORD = 'qwcyibhdjmagovpm'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Application definition
 
