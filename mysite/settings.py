1d18e92927fcfbc8ec7ebf2d6da75714b784d437
Amine <aminesakoute288@gmail.com>
2025-10-12 21:02:17 +0100

    pleaaaaase

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,13 +50,11 @@ DEBUG = True
 ALLOWED_HOSTS = ['.railway.app', '127.0.0.1']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS='False'
+EMAIL_HOST_USER='aminesakoute288@gmail.com'
+EMAIL_HOST_PASSWORD='xmoj usht ouff sbbi'
 
 # Application definition
 
