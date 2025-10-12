933dafbfe3ce31fef8ad122bab4c7ed3cc790fcf
Amine <aminesakoute288@gmail.com>
2025-10-12 14:56:15 +0100

    dw about it again

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
