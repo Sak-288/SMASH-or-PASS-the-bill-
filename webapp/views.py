d53cb812b87b97d1ca9bb6e533dd7fc5e475fbb2
Amine Sakoute <aminesakoute288@gmail.com>
2025-10-12 17:14:36 +0100

    Update views.py
    Fixed index out of range error

--- webapp/views.py
+++ webapp/views.py
@@ -120,7 +120,7 @@ def home(request):
     tempCriteria = float(content[firstRank][6])
     secondChoices = []
     for choice in tempChoices:
-        if abs(float(content[choice][6]) - tempCriteria) <= 50:
+        if abs(float(content[choice - 1][6]) - tempCriteria) <= 50:
             secondChoices.append(choice)
     secondRank = rp.choice(secondChoices)
     
@@ -143,7 +143,7 @@ def home_women(request):
     tempCriteria = float(content[firstRank][6])
     secondChoices = []
     for choice in tempChoices:
-        if abs(float(content[choice][6]) - tempCriteria) <= 50:
+        if abs(float(content[choice - 1][6]) - tempCriteria) <= 50:
             secondChoices.append(choice)
     secondRank = rp.choice(secondChoices)
     
@@ -171,7 +171,7 @@ def home_men(request):
     tempCriteria = float(content[firstRank][6])
     secondChoices = []
     for choice in tempChoices:
-        if abs(float(content[choice][6]) - tempCriteria) <= 50:
+        if abs(float(content[choice - 1][6]) - tempCriteria) <= 50:
             secondChoices.append(choice)
     secondRank = rp.choice(secondChoices)
     
