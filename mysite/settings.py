444d7c0ee81b95dcc3fa15da78afd2e47551d9f7
Amine <aminesakoute288@gmail.com>
2025-10-12 15:31:00 +0100

    changed settings so production ready, no visible secret variables (I hope)

--- mysite/settings.py
+++ mysite/settings.py
@@ -49,12 +49,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['.railway.app']
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'aminesakoute288@gmail.com'
-EMAIL_HOST_PASSWORD = 'qwcyibhdjmagovpm'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Application definition
 
