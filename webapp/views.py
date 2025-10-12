6ecb8423073bc58f3f001d6db976c565435ed034
Amine Sakoute <aminesakoute288@gmail.com>
2025-10-12 19:25:53 +0100

    Update views.py
    Typo in home_women

--- webapp/views.py
+++ webapp/views.py
@@ -139,7 +139,7 @@ def home_women(request):
         content = list(csv.reader(file))
 
     firstRank = rp.choice(WOMEN)
-    tempChoices = [x for x in WOMEN if x != firstRank and i x != 575]
+    tempChoices = [x for x in WOMEN if x != firstRank and if x != 575]
     tempCriteria = float(content[firstRank][6])
     secondChoices = []
     for choice in tempChoices:
