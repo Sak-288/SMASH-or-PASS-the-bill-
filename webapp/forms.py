09139841e22a85b6d7a4995495db107b5ddd2eed
Your Name <aminesakoute288@gmail.com>
2025-10-30 21:26:08 +0100

    continuation of importing issue

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,6 +1,34 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=30)
-    email = forms.EmailField()
-    subject = forms.TextField()
+    name = forms.CharField(
+        max_length=30, 
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Your Name'
+        })
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Your Email'
+        })
+    )
+    subject = forms.CharField(  # Changed from TextField to CharField
+        max_length=200,
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Subject'
+        })
+    )
+    message = forms.CharField(  # Added message field for the actual message
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Your Message',
+            'rows': 5
+        }),
+        required=True
+    )
